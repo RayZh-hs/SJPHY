4da375c3698b395a6a52697bc19ef12ed6ac5ac5
RayZh <rayzhangshanghai@hotmail.com>
2025-11-12 21:05:08 +0800

    feat: finish experiment 2-4

--- lib/graphing/__init__.py
+++ lib/graphing/__init__.py
@@ -1,6 +1,9 @@
 from . import origin # noqa: F401 - import for side effects
 from .utils import plot_graph  # noqa: F401
 
+import os
+os.makedirs("output", exist_ok=True)
+
 __all__ = [
     "origin",
     "plot_graph",
